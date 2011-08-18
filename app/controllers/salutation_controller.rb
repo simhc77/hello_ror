a08eed2d3d3c135754eee405ed24c66ff22902c4
simhc77 <alex.simhc@gmail.com>
2011-08-18 11:13:11 +0900

    initial commit

---
+++ app/controllers/salutation_controller.rb
@@ -0,0 +1,2 @@
+class SalutationController < ApplicationController
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = '2e175a546aa32390f1c2fee3a2f167c6de5aaf4243393f06b0279860118b876dc321b883315327f893ef62020dcb9152c9d15233d6f0626af92a7a5061eb8c12'
